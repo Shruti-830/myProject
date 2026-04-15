f28fa54a61406e6f61d506fbf35d484299776464
Shruti-830 <shrutika.sfdc@gmail.com>
2026-04-15 17:54:25 +0530

    Designed using Tailwind css install,import and use in app.jsx for making card and button

---
+++ Module12-Design/Tailwind-css/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-4">
+      
+    
+      <h1 className="text-3xl font-bold">Design Tailwind Css</h1>
+
+      <div className="bg-white p-6 rounded-xl h-40 w-40 text-center flex items-center">
+        Inside the card
+      </div>
+
+    
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+        Click me
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
