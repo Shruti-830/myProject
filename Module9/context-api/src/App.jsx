18656b64a89c7939e32269bcf41ffc13f9c446a1
Shruti-830 <shrutika.sfdc@gmail.com>
2026-04-22 17:53:54 +0530

    print username using context api

--- Module9/context-api/src/App.jsx
+++ Module9/context-api/src/App.jsx
@@ -1,31 +1,38 @@
-// import React, { createContext,useState,useContext } from "react";
+// import React, { useState } from "react";
+// import { createContext, useContext } from "react";
 
+// // step 1- creating a context
 // const counterContext = createContext();
 
-// function countContextProvider({children}){
-//     const [count, setCount] = useState(0);
-//   return(
+// // step - 2 : create a context provider
+
+// function CountContextProvider({ children }) {
+//   const [count, setCount] = useState(0); // 0
+
+//   return (
 //     <>
-//       <counterContext.Provider value ={{count , setCount}}>
+//       <counterContext.Provider value={{ count, setCount }}>
 //         {children}
 //       </counterContext.Provider>
 //     </>
 //   );
 // }
 
-// function Parent(){
-//   return(
-//     <countContextProvider>
-//       <Value/>
-//       <Increase/>
-//       <Decrease/>
-//     </countContextProvider>
+// function Parent() {
+//   return (
+//     <CountContextProvider>
+//       <Value />
+//       <Increase />
+//       <Decrease />
+//     </CountContextProvider>
 //   );
 // }
 
+// // step 3 - consuming the logic in components
 
 // function Value() {
 //   const { count } = useContext(counterContext);
+
 //   return (
 //     <>
 //       <h1>Count : {count}</h1>
@@ -35,6 +42,7 @@
 
 // function Increase() {
 //   const { count, setCount } = useContext(counterContext);
+
 //   return (
 //     <>
 //       <button onClick={() => setCount(count + 1)}>Increase</button>
@@ -44,6 +52,7 @@
 
 // function Decrease() {
 //   const { count, setCount } = useContext(counterContext);
+
 //   return (
 //     <>
 //       <button onClick={() => setCount(count - 1)}>Decrease</button>
@@ -51,11 +60,11 @@
 //   );
 // }
 
-// function App(){
-//   return(
+// function App() {
+//   return (
 //     <div>
-//       <h1>Counter App using Context Api</h1>
-//       <Parent/>
+//       <h1>Counter app with context api </h1>
+//       <Parent />
 //     </div>
 //   );
 // }
@@ -65,69 +74,42 @@
 import React, { useState } from "react";
 import { createContext, useContext } from "react";
 
-// step 1- creating a context
-const counterContext = createContext();
+const userContext = createContext();
 
-// step - 2 : create a context provider
-
-function CountContextProvider({ children }) {
-  const [count, setCount] = useState(0); // 0
-
-  return (
-    <>
-      <counterContext.Provider value={{ count, setCount }}>
-        {children}
-      </counterContext.Provider>
-    </>
-  );
+function UserContextProvider({children}){
+  const userName="Shruti";
+  return(
+  <>
+    <userContext.Provider value={userName}>
+      {children}
+    </userContext.Provider>
+  </>
+  )
 }
 
-function Parent() {
-  return (
-    <CountContextProvider>
-      <Value />
-      <Increase />
-      <Decrease />
-    </CountContextProvider>
-  );
+function Parent (){
+  return(
+    <>
+      <UserContextProvider>
+        <Value/>
+      </UserContextProvider>
+    </>
+  )
 }
 
-// step 3 - consuming the logic in components
-
-function Value() {
-  const { count } = useContext(counterContext);
-
-  return (
+function Value(){
+  const userName = useContext(userContext);
+  return(
     <>
-      <h1>Count : {count}</h1>
+      <h1>User Name :{userName}</h1>
     </>
-  );
-}
-
-function Increase() {
-  const { count, setCount } = useContext(counterContext);
-
-  return (
-    <>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-    </>
-  );
-}
-
-function Decrease() {
-  const { count, setCount } = useContext(counterContext);
-
-  return (
-    <>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-    </>
-  );
+  )
 }
 
 function App() {
   return (
     <div>
-      <h1>Counter app with context api </h1>
+      <h1>User app with context api </h1>
       <Parent />
     </div>
   );
